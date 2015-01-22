aa3e6b9a71fb94a98f3b00252de7da5e78ea3541
Harshil Garg <harshilgarg51@gmail.com>
2015-01-22 16:12:29 -0500

    Added the NAVX-MXP interface. Requires testing on robot.

--- src/org/usfirst/frc/team2869/robot/Robot.java
+++ src/org/usfirst/frc/team2869/robot/Robot.java
@@ -2,14 +2,18 @@
 package org.usfirst.frc.team2869.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import org.usfirst.frc.team2869.robot.commands.ExampleCommand;
-
 import org.usfirst.frc.team2869.robot.subsystems.DriveTrain;
 import org.usfirst.frc.team2869.robot.subsystems.ExampleSubsystem;
+
+import com.kauailabs.nav6.frc.IMUAdvanced;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -19,6 +23,11 @@ import org.usfirst.frc.team2869.robot.subsystems.ExampleSubsystem;
  * directory.
  */
 public class Robot extends IterativeRobot {
+	
+	SerialPort serialport;
+	IMUAdvanced imu;
+	
+	boolean initialization;
 
 	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
 	public static final DriveTrain chassis = new DriveTrain();
@@ -26,12 +35,29 @@ public class Robot extends IterativeRobot {
 	public static OI oi;
 
     Command autonomousCommand;
-
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
     public void robotInit() {
+    	
+      	try {
+        	serialport = new SerialPort(57600, SerialPort.Port.kMXP);
+    		imu = new IMUAdvanced(serialport, (byte) 50);
+        	} catch( Exception ex ) {
+        		
+        	}
+            if ( imu != null ) {
+                LiveWindow.addSensor("IMU", "Gyro", imu);
+            }
+            initialization = true;
+            
+            if (initialization && !imu.isCalibrating()) {
+                Timer.delay(0.3);
+                imu.zeroYaw();
+                initialization = false;
+            }
+    	
 		oi = new OI();
         autonomousCommand = new ExampleCommand();
     }
@@ -49,6 +75,19 @@ public class Robot extends IterativeRobot {
     }
 
     public void teleopInit() {
+    	SmartDashboard.putBoolean("IMU_Connected",      imu.isConnected());
+        SmartDashboard.putBoolean("IMU_IsCalibrating",  imu.isCalibrating());
+        SmartDashboard.putNumber( "IMU_Yaw",            imu.getYaw());
+        SmartDashboard.putNumber( "IMU_Pitch",          imu.getPitch());
+        SmartDashboard.putNumber( "IMU_Roll",           imu.getRoll());
+        SmartDashboard.putNumber( "IMU_CompassHeading", imu.getCompassHeading());
+        SmartDashboard.putNumber( "IMU_Update_Count",   imu.getUpdateCount());
+        SmartDashboard.putNumber( "IMU_Byte_Count",     imu.getByteCount());
+        SmartDashboard.putNumber( "IMU_Accel_X",        imu.getWorldLinearAccelX());
+        SmartDashboard.putNumber( "IMU_Accel_Y",        imu.getWorldLinearAccelY());
+        SmartDashboard.putBoolean("IMU_IsMoving",       imu.isMoving());
+        SmartDashboard.putNumber( "IMU_Temp_C",         imu.getTempC());
+
 		if (autonomousCommand != null) autonomousCommand.cancel();
     }
 
